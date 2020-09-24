ed6a2876f92d27ea0cb516cdea376f9a1905975e
lsmith24-school <lsmith24@student.sl.on.ca>
2020-09-24 15:45:46 -0400

    first commit

---
+++ ex1/Ex1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+// Lab exercise #1
+// Name: Lauren Smith
+int main()
+{ 
+	
+	cout << "		       ,---.           ,---." << endl;
+	cout << "		      / /\"`.\\.--\"\"\"--./,'\"\\ \\" << endl;
+	cout << "		      \\ \\    _      _     / /" << endl;
+	cout << "		       `./  / __   __ \\  \\,'" << endl;
+	cout << "		       /    /_O)_(_O\\    \\" << endl;
+	cout << "		       |  .-'  ___  `-.  |" << endl;
+	cout << "                     --|       \\_/       |--." << endl;
+	cout << "	          ,'    \\   \\   |   /   /    `" << endl;
+	cout << "                 /       `.  `--^--'  ,'       \\" << endl;
+	cout << "             .-\"\"\"\"\"      `-- .___.--'    .-\"\"\"\"\"-." << endl;
+	cout << ".-----------/         \\------------------/         \\--------------." << endl;
+	cout << "| .---------\\         /----------------- \\         /------------. |" << endl;
+	cout << "| |--------- `-`--`--'   ------====-----  `--'--'-' ------------| |" << endl;
+	cout << "| |------------------------=============------------------------| |" << endl;
+	cout << "| |--------------------====Lab Exercise#1===--------------------| |" << endl;
+	cout << "| |-----------------------===============-----------------------| |" << endl;
+	cout << "| |--------------------------========---------------------------| |" << endl;
+	cout << "| |                                                             | |" << endl;
+	cout << "| | Your task:                                                  | |" << endl;
+	cout << "| | Help me. I am upside down. Please rearrange the cout        | |" << endl;
+	cout << "| | statements to have my feet appear below and my head over    | |" << endl;
+	cout << "| | top.  Don't change the cout statements, just copy and paste | |" << endl;
+	cout << "| | them.                                                       | |" << endl;
+	cout << "| | Show me your completed bear.                                | |" << endl;
+	cout << "| |_____________________________________________________________| |" << endl;
+	cout << "|_________________________________________________________________|" << endl;
+	cout << "                    )__________|_ _|_________(" << endl;
+	cout << "                   |            ||            |" << endl;
+	cout << "                   |____________||____________|" << endl;
+	cout << "                     ),-----.(      ),-----.(" << endl;
+	cout << "                    ,'   ==.  \\    /  .==   `." << endl;
+	cout << "                   (           )  (           )" << endl;
+	cout << "                   `==========='  `==========='" << endl;
+	cout << endl << endl;
+	cout << "Try to make some artwork of your own.  Here's my kitten: ";
+	cout << " =^.";
+	cout << ".^=";
+	cout << endl << endl;
+	system("pause");
+}
